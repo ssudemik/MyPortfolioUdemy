2efc7851d41bb60972ecf4cb70e1d9db4a01da0c
ssudemik <sudeemik23@gmail.com>
2025-04-16 15:53:08 +0300

    The classes were created using the Code First method. The connection to the SQL database was made with migrations, and the tables were added from the classes. A theme was added and used in the project.

---
+++ MyPortfolioUdemy/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.DAL.Context
+{
+    public class MyPortfolioContext : DbContext //dbcontext sınıfına ait proportyleri kullanabilmek için.
+    {
+        //override //mevcuttaki bir metodu ezip istediğimiz formatta kullanabilmek için.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //bağlantı stringinin verdiğim method.
+        {
+            optionsBuilder.UseSqlServer("Server=MODELSUDE;initial Catalog=MyPortfolioDb;integrated Security=true;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        //sınıfları sql'e yansıtma.
+        //code first yapısı kullanarak db bağlantılı işlem yapma.
+
+
+        //About c# daki sınıf ismiyken Abouts sqldeki tablo ismi. Böylece entitiyler birbiri ile karışmayacak.
+        public DbSet<About> Abouts { get; set; } 
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ MyPortfolioUdemy/DAL/Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPortfolioUdemy.DAL.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Adress { get; set; }
+    }
+}
